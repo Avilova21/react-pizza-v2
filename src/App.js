344a405a6708166a08bca46b11025a62ef3471cb
Avilova21 <avilova21dk@gmail.com>
2023-04-19 16:14:42 +0500

    fetching items with backend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
-import pizzas from "./assets/pizza.json"
 
 import "./scss/app.scss";
 
 
 function App() {
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		fetch('https://643f8896b9e6d064bef7b69c.mockapi.io/items')
+			.then((res) => res.json())
+			.then((arr) => {
+				setItems(arr);
+			});
+	}, []);
+
 	return (
 		<div className="wrapper">
 			<Header/>
@@ -21,7 +31,7 @@ function App() {
 					<h2 className="content__title">Все пиццы</h2>
 					<div className="content__items">
 						{
-							pizzas.map((obj) => (
+							items.map((obj) => (
 								<PizzaBlock key={obj.id} {...obj}/>
 							))
 						}
