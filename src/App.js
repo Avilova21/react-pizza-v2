47d10933815ad0e550a3c46365e93d99306b1557
Avilova21 <avilova21dk@gmail.com>
2023-04-24 16:25:37 +0500

    change props drilling to useContext

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import { Routes, Route } from "react-router-dom";
 import Cart from "./pages/Cart"
+import { createContext } from "react";
 
 import "./scss/app.scss";
 
-function App() {
-	const [searchValue, setSearchValue] = useState("")
+export const SearchContext = createContext("");
 
+function App() {
 	return (
 		<div className="wrapper">
-			<Header searchValue={searchValue} setSearchValue={setSearchValue}/>
-			<div className="content">
-				<Routes>
-					<Route path="/" element={<Home searchValue={searchValue}/>}/>
-					<Route path="/cart" element={<Cart/>}/>
-					<Route path="*" element={<NotFound/>}/>
-				</Routes>
-			</div>
+			<SearchContext.Provider value="{searchValue, setSearchValue}">
+				<Header/>
+				<div className="content">
+					<Routes>
+						<Route path="/" element={<Home/>}/>
+						<Route path="/cart" element={<Cart/>}/>
+						<Route path="*" element={<NotFound/>}/>
+					</Routes>
+				</div>
+			</SearchContext.Provider>
 		</div>
 	);
 }
